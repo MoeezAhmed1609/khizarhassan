3092a1f59ac862f402b7cc49c14553d5ac9619f2
MoeezAhmed1609 <MoeezAhmed1609>
2023-10-15 19:29:38 +0500

    Resolved IV

--- frontend/src/components/MainBanner.js
+++ frontend/src/components/MainBanner.js
@@ -8,20 +8,20 @@ import "swiper/css/navigation";
 
 // import required modules
 import { Navigation } from "swiper";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function MainBanner() {
-  // const { data } = useSelector((state) => state.banners);
-  const data = [
-    {
-      md: "https://msznutrition.com/cdn/shop/files/Newbanner1web.jpg?v=1695022546&width=1880",
-      xs: "https://msznutrition.com/cdn/shop/files/Newbanner1.1.jpg?v=1694862636&width=750",
-    },
-    {
-      md: "https://msznutrition.com/cdn/shop/files/Newbanner2web.jpg?v=1695022596&width=1880",
-      xs: "https://msznutrition.com/cdn/shop/files/Newbanner2.jpg?v=1694865180&width=750",
-    },
-  ];
+  const { data } = useSelector((state) => state.banners);
+  // const data = [
+  //   {
+  //     md: "https://msznutrition.com/cdn/shop/files/Newbanner1web.jpg?v=1695022546&width=1880",
+  //     xs: "https://msznutrition.com/cdn/shop/files/Newbanner1.1.jpg?v=1694862636&width=750",
+  //   },
+  //   {
+  //     md: "https://msznutrition.com/cdn/shop/files/Newbanner2web.jpg?v=1695022596&width=1880",
+  //     xs: "https://msznutrition.com/cdn/shop/files/Newbanner2.jpg?v=1694865180&width=750",
+  //   },
+  // ];
   return (
     <>
       <Swiper
@@ -33,8 +33,8 @@ export default function MainBanner() {
         {data?.map((ban, i) => (
           <SwiperSlide key={i}>
             <picture>
-              <source media="(min-width:600px)" srcset={ban.md} />
-              <img alt={`banner${i}`} src={ban.xs} />
+              <source media="(min-width:600px)" srcset={ban?.md} />
+              <img alt={`banner${i}`} src={ban?.xs} />
             </picture>
           </SwiperSlide>
         ))}
