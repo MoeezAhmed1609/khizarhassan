ff32518e25020990d78cab931fb707316231e149
MoeezAhmed1609 <MoeezAhmed1609>
2023-10-16 01:04:28 +0500

    Resolved V

--- frontend/src/components/MainBanner.js
+++ frontend/src/components/MainBanner.js
@@ -12,16 +12,6 @@ import { useSelector } from "react-redux";
 
 export default function MainBanner() {
   const { data } = useSelector((state) => state.banners);
-  // const data = [
-  //   {
-  //     md: "https://msznutrition.com/cdn/shop/files/Newbanner1web.jpg?v=1695022546&width=1880",
-  //     xs: "https://msznutrition.com/cdn/shop/files/Newbanner1.1.jpg?v=1694862636&width=750",
-  //   },
-  //   {
-  //     md: "https://msznutrition.com/cdn/shop/files/Newbanner2web.jpg?v=1695022596&width=1880",
-  //     xs: "https://msznutrition.com/cdn/shop/files/Newbanner2.jpg?v=1694865180&width=750",
-  //   },
-  // ];
   return (
     <>
       <Swiper
